8cf6a44e7b76ce3f3216fe53ceeb0fd40623e028
younsle <dnsdudrla97@naver.com>
2020-09-30 20:04:33 +0900

    threading

--- filefuzz.py
+++ filefuzz.py
@@ -1,3 +1,4 @@
+#-*-coding:utf-8-*-
 from pydbg import *
 from PyQt5.QtWidgets import QApplication, QWidget
 import utils
@@ -35,15 +36,43 @@ class file_fuzzer:
         self.recipients = ['http://dnsdudrla97.github.io/',]
         self.sender     = 'http://dnsdudrla97.github.io/'
         
-        self.test_case  = ["%s%n%s%n%s%n%s%n", "\xff", '\x00', "A"]
+        self.test_case  = ["%s%n%s%n%s%n%s%n", "\xff", '\x00', "A"] # ramda?
 
     def file_picker(self):
         file_list = os.listdir("examples/")
         list_length = len(file_list)
         file = file_list[random.randint(0, list_length-1)]
         shutil.copy("examples\\%s" %file, "test.%s" % self.ext)
-
         return file
+    
+    def fuzz(self):
+        while True:
+            if not self.running:
+                # 먼저 변형을 가할 파일을 선택
+                self.test_file = self.file_picker()
+                self.mutate_file()
+
+                # 디버거 스레드를 실행
+                pydbg_thread = threading.Thread(target=self.start_debugger)
+                pydbg_thread.setDaemon(0)
+                pydbg_thread.start()
+
+                while self.pid == None:
+                    time.sleep(1)
+                
+                # 모니터링 스레드를 실행시킨다.
+                monitor_thread = threading.Thread(target=self.monitor_debugger)
+                monitor_thread.setDaemon(0)
+                monitor_thread.start()
+
+                self.iteration +=1
+            else:
+                time.sleep(1)
+    # 대상 애플리케이션을 실행시키는 디버거 스레드
+
+
+
+
 
 class MyApp(QWidget):
     def __init__(self):

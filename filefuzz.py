3467dbe6063be01db959dc3e22fa9633f55dccc9
younsle <dnsdudrla97@naver.com>
2020-09-30 20:28:23 +0900

    threading-1-2-3

--- filefuzz.py
+++ filefuzz.py
@@ -68,7 +68,72 @@ class file_fuzzer:
                 self.iteration +=1
             else:
                 time.sleep(1)
+
     # 대상 애플리케이션을 실행시키는 디버거 스레드
+    def start_debugger(self):
+        print("[*] Starting debugger for iteration: %d" % self.iteration)
+        self.running = True
+        self.dbg = pydbg()   
+
+        self.dbg.set_callback(EXCEPTION_ACCESS_VIOLATION, self.check_accessv)
+        pid = self.dbg.load(self.exe_path, "test.%s" % self.ext)
+
+        self.pid = self.dbg.pid
+        self.dbg.run()
+    
+    # 에러를 추적하고 해당 정보를 저장하기 위한 접근 위반 핸들러
+    def check_accessv(self, dbg):
+        if dbg.dbg.u.Exception.dwFirstChance:
+            return DBG_CONTINUE
+        print("[*] Woot! Handling an access violation!")
+        self.in_accessv_handler = True
+        crash_bin = utils.crash_binning.crash_binning()
+        crash_bin.record_crash(dbg)
+        self.crash = crash_bin.crash_synopsis()
+    
+        # 에러 정보 작성
+        crash_fd = open("crashes\\crash-%d" % self.iteration, 'w')
+        crash_fd.write(self.crash)
+
+        # 파일 백업
+        shutil.copy("test.%s" % self.ext, "crashes\\%d.%s" % (self.iteration, self.ext))
+        shutil.copy("examples\\%s" % self.test_file, "crashes\\%d_orig.%s" % (self.iteration, self.ext))
+        self.dbg.terminate_process()
+        self.in_accessv_handler = False
+        self.running = False
+        return DBG_EXCEPTION_NOT_HANDLED
+    
+    # 애플리케이션 몇 초 동안 실행시킨담에 종료시키는 모니터링 스레드
+    def monitor_debugger(self):
+        counter = 0
+        print("[*] Monitor thread for pid: %d watting." % self.pid)
+        
+        while counter < 3:
+            time.sleep(1)
+            print(counter)
+            counter += 1
+        
+        if self.in_accessv_handler != True:
+            time.sleep(1)
+            self.dbg.terminate_process()
+            self.pid = None
+            self.running = False
+        else:
+            print("[*] The access violation handler is doing its business. Waiting")
+
+            while self.running:
+                time.sleep(1)
+    # 에러 정보 이메일 통보
+    def notify(self):
+        crash_message = "From:%s\r\n\r\nTo:\r\n\r\nIteration:%d\n\nOutput:\n\n %s" % (self.sender, self.iteration, self.crash)
+        session = smtplib.SMTP(smtpserver)
+        session.sendmail(sender, recipients, crash_message)
+        session.quit()
+        return
+        
+
+
+
 
 
 
